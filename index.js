25a78a5f8cc4874cb2c611ffe3ea245fd4f9c5c4
Jazper <jazpersaldana@yahoo.com>
2023-10-13 14:06:24 -0500

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+const random = Math.ceil(Math.random())
+const num3 = 22
+const num4 = 18
+const mod = num3 % num4
+const max = Math.max(1, 20)
